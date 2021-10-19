4e210c3438dbcea806580e020f640d8068d18f10
PaulaROtero <90134222+PaulaROtero@users.noreply.github.com>
2021-10-19 21:03:45 +0200

    Rama 1 unir master

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm rama 1?')
+    print_hi('PyCharm rama 1??')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
